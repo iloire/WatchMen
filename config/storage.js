f85388f7186717a95afee4d19132efc68ddb5750
HansHammel <oh.schitt@freenet.de>
2017-01-14 09:02:35 +0100

    changed readme, tests

--- config/storage.js
+++ config/storage.js
@@ -4,7 +4,7 @@ module.exports = {
     provider : 'redis',
     options : {
       'redis' : {
-        port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 6379,
+        port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 1216,
         host: process.env.WATCHMEN_REDIS_ADDR_PRODUCTION || '127.0.0.1',
         db: process.env.WATCHMEN_REDIS_DB_PRODUCTION || 1
       }
@@ -31,6 +31,24 @@ module.exports = {
         db: process.env.WATCHMEN_REDIS_DB_TEST || 1
       }
     }
+	/*
+	,retry_strategy: function (options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with a individual error
+            return new Error('The server refused the connection');
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            return new Error('Retry time exhausted');
+        }
+        if (options.times_connected > 10) {
+            // End reconnecting with built in error
+            return undefined;
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000);
+    }
+	*/
   }
 
 };
